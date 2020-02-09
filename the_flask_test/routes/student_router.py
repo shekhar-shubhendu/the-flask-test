e7de38fe9effa18c617c65254d6c3da45bd68bf1
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2020-02-09 12:15:48 +0100

    add api version to route

--- the_flask_test/routes/student_router.py
+++ the_flask_test/routes/student_router.py
@@ -1,20 +1,21 @@
 from flask import jsonify, request
 from the_flask_test.services import student_service
 from the_flask_test.exception import TheFlaskTestException
+from . import API_VERSION
 
 
 def register_routes(app):
-    @app.route("/students/")
+    @app.route(f"/api/{API_VERSION}/students/")
     def get_all_students():
         students = student_service.get_all_students()
         return jsonify([e.serialize() for e in students])
 
-    @app.route("/student", methods=["POST"])
+    @app.route(f"/api/{API_VERSION}/student", methods=["POST"])
     def add_student():
         student = request.get_json()
         return student_service.add_student(student)
 
-    @app.route("/student/<id>", methods=["GET", "DELETE", "PUT"])
+    @app.route(f"/api/{API_VERSION}/student/<id>", methods=["GET", "DELETE", "PUT"])
     def student_id_handler(id: int):
         if not isinstance(int(id), int):
             raise TheFlaskTestException('`id` should be an integer', 400)
